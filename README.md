0484daa22928caaacec232bc609ff1e244ca92d8
aeschalna <aeschalna@gmail.com>
2024-05-10 11:58:31 -0600

    svg generator working for all shapes, readme updated

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # SVG-Generator
 Module 10 Challenge
+
+# Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Test Instructions](#test-instructions)
+- [Walkthrough Videos](#walkthrough-videos)
+- [Questions](#questions)
+
+## Description
+A command line application that allows the user to quickly generate a simple logo, stored as an svg file 
+
+## Installation
+This application is powered by inquirer and jest packages of node.js
+Please run [npm install] to use these packages/this application
+
+## Usage
+Begin application by running node index.js from the command line
+Follow the prompts to generate a logo.svg file!
+
+## Test Instructions
+Once dependencies are installed, the application can be tested with [npm test]
+
+## Walkthrough Videos
+
+
+## Questions
+@fairybones
+aeschalna@gmail.com
+Find me on GitHub, or email me directly!
--- index.js
+++ index.js
@@ -45,101 +45,70 @@ const questions = [
 
 // Create SVG class with methods for rendering
 class Svg {
-    constructor () {
+    constructor() {
         // Initialize text & shape that will have color properties
         this.textEl = ''
         this.shapeEl = ''
     }
     setText(text, color) {
-        this.textEl = `<text x="120 y="100" font-size="40" text-anchor="middle" fill="${color}>${text}</text>`
+        this.textEl = `<text x="120" y="120" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
     }
     setShape(shape) {
         this.shapeEl = shape.render()
     }
-    render () {
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    render() {
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeEl}${this.textEl}</svg>`
     }
 }
 
-// Function to create SVG file and notify user
-function createLogo() {
+// Run application once all files are loaded & prompts inputted
+async function init() {
+    let svgString = '';
     const fileName = 'logo.svg';
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {}
-            console.error(err);
-            return;
+
+    // Prompt user from command line
+    const answers = await inquirer.prompt(questions);
+
+    // Handle text input
+    let text = '';
+    if (answers.text.length > 0 && answers.text.length < 4) {
+        text = answers.text;
+    } else {
+        console.log("Please enter no more than 3 characters");
+        return;
+    }
+    textColor = answers.textColor;
+
+    // Handle shape choice
+    user_shape = answers.shape;
+    shapeColor = answers.shapeColor;
+
+    // Request rendering of shape selected
+    let shapeEl;
+    if (user_shape === "Circle" || user_shape === "circle") {
+        shapeEl = new Circle();
+    } else if (user_shape === "Square" || user_shape === "square") {
+        shapeEl = new Square();
+    } else if (user_shape === "Triangle" || user_shape === "triangle") {
+        shapeEl = new Triangle();
+    } else {
+        console.log("Oops! Something went wrong.")
+    }
+    shapeEl.setColor(shapeColor);
+
+    // Function to create SVG file 
+    function createLogo() {
+
+        fs.writeFile(fileName, svgString, (err) => {
+            err ? console.error('Oops! Something went wrong.') : console.log(`Generated ${fileName}!`);
         });
-        console.log(`Generated ${fileName}!`);
+    }
+
+    // Append data to SVG
+    let svg = new Svg();
+    svg.setText(text, textColor);
+    svg.setShape(shapeEl);
+    svgString = svg.render();
+    createLogo(fileName, svgString);
 }
-
-// Prompt user from command line
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter up to 3 characters:',
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter a text color (Keyword or Hexadecimal #)!',
-        },
-        {
-            type: 'list',
-            name: 'shape',
-            message: 'Choose a background shape:',
-            choices: [
-                {
-                    name: 'Circle',
-                    value: 'circle'
-                },
-                {
-                    name: 'Square',
-                    value: 'square'
-                },
-                {
-                    name: 'Triangle',
-                    value: 'triangle'
-                }
-            ]
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter a color keyword or hex # for the shape you selected:',
-        }
-    ])
-    .then((answers) => {
-        // text input
-        let text = '';
-        if (answers.text.length > 0 && answers.text.length < 4) {
-            text = answers.text;
-        } else {
-            console.log("Please enter no more than 3 characters");
-            return;
-        }
-        // color inputs
-        textColor = answers.textColor;
-        shapeColor = answers.shapeColor;
-        // shape input
-        user_shape = answers.shape;
-
-        let shapeEl; // grab user choice to create new shape
-        if (user_shape === "Circle" || user_shape === "circle") {
-            shapeEl = new Circle();
-        } else if (user_shape === "Square" || user_shape === "square") {
-            shapeEl = new Square(); 
-        } else if (user_shape === "Triangle" || user_shape === "triangle") {
-            shapeEl = new Triangle();
-        } else {
-            console.log("Oops! Something went wrong.")
-        }
-        shapeEl.setColor(shapeColor);
-
-        let svg = new Svg();
-        svg.setText(text, textColor);
-        svg.setShape(shapeEl);
-        svgString = svg.render();
-        createLogo(fileName, svgString);
-    })
+init();
